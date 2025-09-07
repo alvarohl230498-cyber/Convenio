d0ca778af6888ed182ad7a4f28b5616a83cb9389
alrohela <alvarohl9804@outlook.com>
2025-09-07 16:58:30 -0500

    fit rutas html 27

--- convenios/routes.py
+++ convenios/routes.py
@@ -6,8 +6,9 @@ convenios_bp = Blueprint("convenios", __name__, url_prefix="/convenios")
 
 
 # /convenios/  -> Lista de EMPLEADOS (index del módulo)
-@convenios_bp.get("/", endpoint="index")
-@convenios_bp.get("") 
+
+@convenios_bp.get("",endpoint = "index")
+@convenios_bp.get("/", endpoint="index-slash")
 @login_required
 def empleados_index():
     empleados = Empleado.query.order_by(Empleado.nombre).all()
